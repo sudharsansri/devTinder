349ab37f90bd4b1c895a702da2388a1590952cb7
sudharsan <sudharsanravi10@gmail.com>
2025-06-30 22:10:55 +0530

    method

--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();  
 const port = 3000;  
 
-app.use("/use",(req, res, next) => {
+app.get("/use",(req, res, next) => {
     res.send('Hello, World! ');
+})
+app.post("/user",(req, res, next) => {
+    res.send('Hello, Worldffff! ');
+})
+app.use("/",(req, res, next) => {
+    res.send('Hello, World! sri ');
 })
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
+
+//order of writing the codeis important
 
